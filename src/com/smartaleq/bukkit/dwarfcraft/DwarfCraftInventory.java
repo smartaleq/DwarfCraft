aa6c316ccb4b38e977ec6d10c67d25b21de85369
smartaleq <joeybeckman@gmail.com>
2011-01-21 01:49:38 -0800

    version .5

---
+++ src/com/smartaleq/bukkit/dwarfcraft/DwarfCraftInventory.java
@@ -0,0 +1,18 @@
+package com.smartaleq.bukkit.dwarfcraft;
+
+import org.bukkit.entity.Player;
+
+public class DwarfCraftInventory {
+
+	public static int countInventoryItems(int itemId, Player player) {
+		for(int itemSlot =0; itemSlot < 100; itemSlot++){
+			
+		}
+		return 0;
+	}
+
+	public static void removeInventoryItems(int itemId, int count, Player player){
+		
+		
+	}
+}
